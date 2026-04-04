384b92e1df4a443b3eabd76e56d741dc54023e55
Devashish Soni <76209941+SwaggyXO@users.noreply.github.com>
2026-04-04 16:09:01 +0530

    Add: duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        int numsLen = nums.size();
+        map<int, int> numMap;
+        for (int i = 0; i < numsLen; i++) {
+            numMap[nums[i]]++;
+        }
+
+        for (auto numItr:numMap) {
+            if (numItr.second >= 2) return 1;
+        }
+
+        return 0;
+    }
+};
