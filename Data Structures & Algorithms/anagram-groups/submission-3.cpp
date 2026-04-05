b6b61602093d9171cbbde5601d13277a10e15cc2
Devashish Soni <76209941+SwaggyXO@users.noreply.github.com>
2026-04-05 15:27:47 +0530

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        // init - empty result container - map<str, set<str>>
+        // iter through strs -> init set before inserting the str as key
+        // in the map.
+        // if key is not seen, add it, if it is seen, update the set
+        // voila!
+        // not voila..
+        // use the logic from valid anagrams and the fact
+        // that strs[i] is fixed to 26 chars only.
+        // create a hash by adding the char values in int, and use it as
+        // the key for the groups.
+        // now voila!
+        // can be optimized I believe, need to think more.
+        // wrong. on so many accounts. simple addition is not it.
+        // need to think more about edge cases. really lacking there.
+        // going back to the thing I had in find right away - sorting.
+        // ok. somehow the sorting solution is the fastest?
+        // I will dig more.
+
+        unordered_map<string, vector<string>> strsMap;
+        vector<vector<string>> result;
+        for (int i = 0; i < strs.size(); i++) {
+            string currStr = strs[i];
+            sort(currStr.begin(), currStr.end());
+            if (strsMap.find(currStr) != strsMap.end()) {
+                strsMap[currStr].push_back(strs[i]);
+                continue;
+            }
+            vector<string> tmp;
+            tmp.push_back(strs[i]);
+            strsMap[currStr] = tmp;
+        }
+        for (auto x:strsMap) {
+            result.push_back(x.second);
+        }
+
+        return result;
+    }
+};
