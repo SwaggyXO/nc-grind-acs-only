2c9dea98f1a05727d162f68f20980fe176e97265
Devashish Soni <76209941+SwaggyXO@users.noreply.github.com>
2026-06-08 22:46:00 +0530

    Add: invert-a-binary-tree - submission-1

---
+++ Data Structures & Algorithms/invert-a-binary-tree/submission-1.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        // 1 2 3 4 5 6 7 - level order traversal basically.
+        // lvl 1 - no flip coz root bruh
+        // lvl 2 onwards - swap: (first, last), (first + 1, last - 1)
+        // and so on.
+        // 1 3 2 7 6 5 4
+        if (!root || (!root->left && !root->right)) return root;
+        queue<TreeNode*> nodes;
+        nodes.push(root);
+        while (!nodes.empty()) {
+            int levelSize = nodes.size();
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode* curr = nodes.front();
+                TreeNode* temp = curr->left;
+                curr->left = curr->right;
+                curr->right = temp;
+                if (curr->left) nodes.push(curr->left);
+                if (curr->right) nodes.push(curr->right);
+                nodes.pop();
+            }
+        }
+        return root;
+    }
+};
