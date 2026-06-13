69cff8e419d567cc74d03291b9d99e1c8c674819
Devashish Soni <76209941+SwaggyXO@users.noreply.github.com>
2026-06-13 16:57:02 +0530

    Add: subtree-of-a-binary-tree - submission-1

---
+++ Data Structures & Algorithms/subtree-of-a-binary-tree/submission-1.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (!p && !q) return true;
+        if (!p || !q) return false;
+
+        if (p->val != q->val) return false;
+        bool isLeftOk = isSameTree(p->left, q->left);
+        bool isRightOk = isSameTree(p->right, q->right);
+
+        if (!isLeftOk || !isRightOk) return false;
+        return true;
+    }
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        // thought it would be easier with iterative
+        // ended up implementing recursive first.
+        // i don't even understand it fully yet 💀
+
+        if (!root && !subRoot) return true;
+        if (!root || !subRoot) return false;
+
+        if (root->val == subRoot->val && isSameTree(root, subRoot)) {
+            return true;
+        }
+
+        bool isPresentInLeft = isSubtree(root->left, subRoot);
+        bool isPresentInRight = isSubtree(root->right, subRoot);
+
+        return isPresentInLeft || isPresentInRight;
+    }
+};
